aa585bcc4e3a9b0fcc72140c03e8afbe57f0c8ac
GusTiavo123 <luagus0@gmail.com>
2024-04-17 14:02:07 -0300

    Add functionality to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,25 +1,51 @@
-import Image from "next/image";
-import styles from "./contact.module.css"
-import { Metadata } from "next";
+"use client";
 
-export const metadata: Metadata = {
-  title: "IdeaNova contact page",
-  description: "IdeaNova contact form",
-};
+import Image from "next/image";
+import styles from "./contact.module.css";
+import toast from "react-hot-toast";
 
 const ContactPage = () => {
+  const handleContact = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+
+    toast.success("Message has been sent");
+
+    form.reset();
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image src="/contact.png" alt="" fill />
       </div>
       <div className={styles.formContainer}>
-        <form className={styles.form}>
-          <input type="text" placeholder="Name and Surname" />
-          <input type="email" placeholder="Email Address" />
+        <form className={styles.form} onSubmit={handleContact}>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            required
+            placeholder="Name and Surname"
+          />
+          <input
+            type="email"
+            placeholder="Email Address"
+            id="email"
+            name="email"
+            required
+          />
           <input type="tel" placeholder="Phone Number (optional)" />
-          <textarea name="" id="" cols={30} rows={10} placeholder="Message"></textarea>
-          <button className={styles.button}>Send</button>
+          <textarea
+            name="message"
+            id="message"
+            cols={30}
+            rows={10}
+            placeholder="Message"
+          ></textarea>
+          <button className={styles.button} type="submit">
+            Send
+          </button>
         </form>
       </div>
     </div>
